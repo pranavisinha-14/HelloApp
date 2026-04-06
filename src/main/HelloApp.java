f700420d243f0df1db9a962764e4038683182924
pranavisinha-14 <sinha.pranavi14@gmail.com>
2026-04-06 11:09:41 +0530

    Coded UC2: Display Hello with Command-Line Argument

--- src/main/HelloApp.java
+++ src/main/HelloApp.java
@@ -1,7 +1,13 @@
 public class HelloApp {
-//uc1
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
 
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
+
+    }
 }
